370f33884d6dff10b745efbc36046a1b5898b049
Adam Olivant <24426@longroad.ac.uk>
2013-11-12 09:49:45 +0000

    Created Window
    Window appears with data view.

---
+++ main_controller.py
@@ -0,0 +1,46 @@
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyQt4.QtSql import *
+
+import sys
+
+class MainWindow(QMainWindow):
+    """simple example using QtSql"""
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Coffee Shop SQL with PyQt")
+
+        self.db = QSqlDatabase.addDatabase("QSQLITE")
+        self.db.setDatabaseName("coffeeshop.db")
+        self.db.open()
+
+        self.table_view = QTableView()
+        self.submit_changes_button = QPushButton("Submit Changes")
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.table_view)
+        self.layout.addWidget(self.submit_changes_button)
+
+        self.widget = QWidget()
+        self.widget.setLayout(self.layout)
+
+        self.setCentralWidget(self.widget)
+
+        self.create_table_model()
+
+    def create_table_model(self):
+        self.model = QSqlTableModel()
+        self.model.setTable("Customer")
+        self.table_view.setModel(self.model)
+        self.table_view.model().select()
+
+if __name__ == "__main__":
+    application = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    window.raise_()
+    application.exec_()
+        
+
+        
